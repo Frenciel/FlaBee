4ae760d90ec5d1296685a275ee399e4a8f592806
Frenciel Anggi <frencielanggi02@gmail.com>
2022-06-16 19:57:25 +0100

    Flappy Bee

---
+++ lib/bee.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyBee extends StatelessWidget {
+  final beeY;
+  final double beeWidth; // normal double value for width
+  final double beeHeight; // out of 2
+
+  MyBee({this.beeY, required this.beeWidth, required this.beeHeight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment(0, (2 * beeY + beeHeight) / (2 - beeHeight)),
+      child: Image.asset(
+        'lib/images/bee.png',
+        width: MediaQuery.of(context).size.height * beeWidth * 3 / 5,
+        height: MediaQuery.of(context).size.height * beeHeight * 3 / 5,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,332 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/barrier.dart';
+import 'package:flutter_application_1/bee.dart';
+
+// to do
+// 1. fix barrier hit bug
+// 2. add sound / music
+// 3. add different height of barrier
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // bee variables
+  static double beeY = 0;
+  double initialPos = beeY;
+  double height = 0;
+  double time = 0;
+  double gravity = -0.045; // how strong the gravity is
+  double velocity = 0.35; // how strong the jump is
+  double beeWidth = 0.1;
+  double beeHeight = 0.1;
+
+  // game settings
+  bool gameHasStarted = false;
+  int score = 0;
+  int highscore = 0;
+
+  // barrier variables
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5; // out of 2
+  List<List<double>> barrierHeight = [
+    // out of 2 (entire height of screen)
+    // [topHeight, bottomHeight]
+    [0.6, 0.4],
+    [0.4, 0.6],
+    // [0.2, 0.8],
+    // [0.8, 0.2],
+  ];
+
+  void startGame() {
+    gameHasStarted = true;
+
+    Timer.periodic(Duration(milliseconds: 5), (timer) {
+      // a real physical jump follows the arc of a parabola
+      // according to a simple quadratic equation
+      height = gravity * time * time + velocity * time;
+
+      setState(() {
+        beeY = initialPos - height;
+      });
+
+      // check if bee is dead
+      if (beeIsDead()) {
+        timer.cancel();
+        _showDialog();
+      }
+
+      // move map (barriers)
+      moveMap();
+
+      // keep the time going
+      time += 0.05;
+    });
+  }
+
+  void moveMap() {
+    for (int i = 0; i < barrierX.length; i++) {
+      // move barriers
+      setState(() {
+        barrierX[i] -= 0.005;
+      });
+
+      // if barrier exit map, loop
+      if (barrierX[i] < -1.5) {
+        score += 1;
+        // 2 for 2 types of barrier and 1.5 for if check above
+        barrierX[i] += 2 * 1.5;
+      }
+    }
+  }
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialPos = beeY;
+    });
+  }
+
+  void resetGame() {
+    setState(() {
+      if (score > highscore) {
+        highscore = score;
+      }
+      score = 0;
+      beeY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = beeY;
+      barrierX = [2, 2 + 1.5];
+      barrierHeight = [
+        [0.6, 0.4],
+        [0.4, 0.6],
+        // [0.2, 0.8],
+        // [0.8, 0.2],
+      ];
+    });
+  }
+
+  void _showDialog() {
+    showCupertinoDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            // backgroundColor: Colors.brown,
+            title: Center(
+              child: Text(
+                "G A M E  O V E R",
+              ),
+            ),
+            content: Center(
+              child: Text(
+                "Score: " + score.toString(),
+              ),
+            ),
+            actions: [
+              CupertinoDialogAction(
+                  child: Text("Play again"),
+                  onPressed: () {
+                    Navigator.of(context).pop(); //dismiss alert dialog
+                    resetGame();
+                  }),
+              CupertinoDialogAction(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    resetGame();
+                  }),
+            ],
+          );
+        });
+  }
+
+  bool beeIsDead() {
+    // check if bee hits top or bottom of screen
+    if (beeY < -1 || beeY > 1) {
+      return true;
+    }
+
+    // if bee hits barrier
+    // check if bee is within x and y coordinates of barrier
+    for (int i = 0; i < barrierX.length; i++) {
+      if (barrierX[i] <= beeWidth &&
+          barrierX[i] + beeWidth >= -beeWidth &&
+          (beeY <= -1 + barrierHeight[i][0] ||
+              beeY + beeHeight >= 1 - barrierHeight[i][1])) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: gameHasStarted ? jump : startGame,
+      child: Scaffold(
+        body: Column(children: [
+          Expanded(
+            flex: 5,
+            child: Container(
+              color: Colors.blue.shade400,
+              child: Center(
+                child: Stack(
+                  children: [
+                    // the bee
+                    MyBee(
+                      beeY: beeY,
+                      beeHeight: beeHeight,
+                      beeWidth: beeWidth,
+                    ),
+
+                    // Top barrier 0
+                    MyBarrier(
+                      barrierX: barrierX[0],
+                      barrierHeight: barrierHeight[0][0],
+                      barrierWidth: barrierWidth,
+                      isThisBottomBarrier: false,
+                    ),
+
+                    // Bottom barrier 0
+                    MyBarrier(
+                      barrierX: barrierX[0],
+                      barrierHeight: barrierHeight[0][1],
+                      barrierWidth: barrierWidth,
+                      isThisBottomBarrier: true,
+                    ),
+
+                    // Top barrier 1
+                    MyBarrier(
+                      barrierX: barrierX[1],
+                      barrierHeight: barrierHeight[1][0],
+                      barrierWidth: barrierWidth,
+                      isThisBottomBarrier: false,
+                    ),
+
+                    // Bottom barrier 1
+                    MyBarrier(
+                      barrierX: barrierX[1],
+                      barrierHeight: barrierHeight[1][1],
+                      barrierWidth: barrierWidth,
+                      isThisBottomBarrier: true,
+                    ),
+
+                    // // Top barrier 2
+                    // MyBarrier(
+                    //   barrierX: barrierX[2],
+                    //   barrierHeight: barrierHeight[2][0],
+                    //   barrierWidth: barrierWidth,
+                    //   isThisBottomBarrier: false,
+                    // ),
+
+                    // // Bottom barrier 2
+                    // MyBarrier(
+                    //   barrierX: barrierX[2],
+                    //   barrierHeight: barrierHeight[2][1],
+                    //   barrierWidth: barrierWidth,
+                    //   isThisBottomBarrier: true,
+                    // ),
+
+                    // // Top barrier 3
+                    // MyBarrier(
+                    //   barrierX: barrierX[3],
+                    //   barrierHeight: barrierHeight[3][0],
+                    //   barrierWidth: barrierWidth,
+                    //   isThisBottomBarrier: false,
+                    // ),
+
+                    // // Bottom barrier 3
+                    // MyBarrier(
+                    //   barrierX: barrierX[3],
+                    //   barrierHeight: barrierHeight[3][1],
+                    //   barrierWidth: barrierWidth,
+                    //   isThisBottomBarrier: true,
+                    // ),
+
+                    Container(
+                      alignment: Alignment(0, -0.5),
+                      child: Text(
+                        gameHasStarted ? '' : 'T A P   T O   P L A Y',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 15,
+            color: Colors.green,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.brown.shade600,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "S C O R E",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      Text(
+                        score.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "B E S T",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 17,
+                      ),
+                      Text(
+                        highscore.toString(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
